038fa927f7a88f07fdd5c28c233c046f560f52a7
Max Abramov <jailthehorny@gmail.com>
2024-09-18 10:56:37 +0300

    Fix floating point dumper

--- encodings/numeric/ieee754.c
+++ encodings/numeric/ieee754.c
@@ -1,46 +1,45 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
-#include <stdlib.h>
 
-void FormatBin(uint64_t x, size_t len, char buf[]) {
-  for (size_t i = 0; i < len; ++i) {
+void format_binary(uint64_t x, int bits, char buf[]) {
+  for (int i = 0; i < bits; ++i) {
     if (x & (1ULL << i)) {
-      buf[i] = '1';
+      buf[bits - 1 - i] = '1';
     } else {
-      buf[i] = '0';
+      buf[bits - 1 - i] = '0';
     }
   }
 
-  buf[len] = '\0';
+  buf[bits] = '\0';
 }
 
-void PrintDoubleRepr(double x) {
+void dump_double(double x) {
   uint64_t bits;
   memcpy(&bits, &x, sizeof(bits));
 
   uint64_t sign = bits >> 63;
   uint64_t exp = (bits << 1) >> 53;
-  uint64_t mant = (bits << 12) >> 12;
+  uint64_t mantissa = (bits << 12) >> 12;
 
   printf("x = %f\n", x);
 
   char buf[64];
-  FormatBin(sign, 1, buf);
+  format_binary(sign, 1, buf);
   printf("sign = %s\n", buf);
-  FormatBin(exp, 11, buf);
+  format_binary(exp, 11, buf);
   printf("exp = %s\n", buf);
-  FormatBin(mant, 52, buf);
-  printf("mant = %s\n", buf);
+  format_binary(mantissa, 52, buf);
+  printf("mantissa = %s\n", buf);
 
   printf("\n");
 }
 
 int main() {
-  PrintDoubleRepr(2 * 1.23456);
-  PrintDoubleRepr(-1.23456);
-  PrintDoubleRepr(1.23 / 0.0);
-  PrintDoubleRepr(-1.23 / 0.0);
-  PrintDoubleRepr(0.0 / 0.0);
-  PrintDoubleRepr(1e-312);
+  dump_double(2 * 1.23456);
+  dump_double(-1.23456);
+  dump_double(1.23 / 0.0);
+  dump_double(-1.23 / 0.0);
+  dump_double(0.0 / 0.0);
+  dump_double(1e-312);
 }
