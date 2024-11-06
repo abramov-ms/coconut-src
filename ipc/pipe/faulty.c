280ba1958c1a9b6e0022ba35550bda8145d7832b
Max Abramov <jailthehorny@gmail.com>
2024-11-06 15:35:40 +0300

    Bump

--- ipc/pipe/faulty.c
+++ ipc/pipe/faulty.c
@@ -9,6 +9,7 @@ int main(int argc, const char* argv[]) {
   if (argc != 3) {
     return 1;
   }
+
   const char* exe1 = argv[1];
   const char* exe2 = argv[2];
 
@@ -21,15 +22,17 @@ int main(int argc, const char* argv[]) {
     execlp(exe1, exe1, NULL);
   }
 
+  wait(NULL);
+
   if (fork() == 0) {
     close(pipe_fds[1]);
     dup2(pipe_fds[0], STDIN_FILENO);
     execlp(exe2, exe2, NULL);
   }
+
   close(pipe_fds[0]);
   close(pipe_fds[1]);
 
-  wait(NULL);
   wait(NULL);
 
   return 0;
--- ipc/pipe/main.c
+++ ipc/pipe/main.c
@@ -16,11 +16,6 @@ int main(int argc, const char* argv[]) {
   pid_t last;
   int prev_stdout;
   int pipe_fds[2];
-  // pipe
-  // -> 1 -> 0 -> parent
-  // -> 1 -> 0 -> child
-
-  // stdin -> proc1 -> pipe1 -> proc2 -> pipe2 -> proc3 -> stdout
 
   for (int i = 0; i < n_commands; ++i) {
     if (i < n_commands - 1) {
@@ -37,17 +32,17 @@ int main(int argc, const char* argv[]) {
           dup2(prev_stdout, STDIN_FILENO);
         }
         if (i < n_commands - 1) {
-          // close(pipe_fds[0]);
+          close(pipe_fds[0]);
           dup2(pipe_fds[1], STDOUT_FILENO);
         }
         execlp(commands[i], commands[i], NULL);
         err(EXIT_FAILURE, "exec");
       default:
         if (i > 0) {
-          // close(prev_stdout);
+          close(prev_stdout);
         }
         if (i < n_commands - 1) {
-          // close(pipe_fds[1]);
+          close(pipe_fds[1]);
           prev_stdout = pipe_fds[0];
         }
     }
@@ -62,5 +57,6 @@ int main(int argc, const char* argv[]) {
   if (WIFEXITED(status)) {
     return WEXITSTATUS(status);
   }
+
   return EXIT_FAILURE;
 }
