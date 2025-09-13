7379bafe868efe044ac7b8d8bffb1c0f0ea5f220
Max Abramov <jailthehorny@gmail.com>
2025-09-13 11:42:43 +0300

    tidy up

--- intro/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int invoke_ub(int y);
-
-int main(int argc, char* argv[]) {
-  printf("%d\n", invoke_ub(argc));
-  return 0;
-}
--- intro/ub.c
+++
@@ -1,5 +0,0 @@
-int invoke_ub(int y) {
-  int x = 0x7fffffff;
-  x += y;
-  return x;
-}
