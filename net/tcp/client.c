9e77998c4eb4235e1c834d8182e9da91cc651644
Max Abramov <jailthehorny@gmail.com>
2025-11-22 14:48:01 +0300

    update tcp client & server

--- net/tcp/client.c
+++ net/tcp/client.c
@@ -1,107 +1,99 @@
-#include <err.h>
-#include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <err.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 
-int dial_tcp(const char* ip_str, const char* port_str) {
-  int server_fd;
-  if ((server_fd = socket(AF_INET, SOCK_STREAM, /*protocol=*/0)) == -1) {
+#include <arpa/inet.h>
+#include <sys/un.h>
+
+int new_socket() {
+  int fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (fd == -1) {
     err(EXIT_FAILURE, "socket");
   }
 
-  struct in_addr ip = {};
-  if (inet_aton(ip_str, &ip) == 0) {
-    fprintf(stderr, "bad ip address");
-    exit(EXIT_FAILURE);
-  }
-  char* end_ptr;
-  in_port_t port = strtol(port_str, &end_ptr, /*base=*/10);
-  if (*end_ptr != '\0') {
-    fprintf(stderr, "bad tcp port");
-    exit(EXIT_FAILURE);
-  }
-  struct sockaddr_in addr = {
-      .sin_family = AF_INET, .sin_port = htons(port), .sin_addr = ip};
-  if (connect(server_fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-    err(EXIT_FAILURE, "connect");
-  }
-
-  return server_fd;
+  return fd;
 }
 
-void send_all(int sock, char* buffer, size_t count) {
-  size_t done = 0;
-  while (done < count) {
-    ssize_t bytes = send(sock, buffer + done, count - done, /*flags=*/0);
-    if (bytes == -1) {
-      err(EXIT_FAILURE, "send");
-    }
-    done += bytes;
-  }
-}
-
-void write_all(int fd, char* buffer, size_t count) {
-  size_t done = 0;
-  while (done < count) {
-    ssize_t bytes = write(fd, buffer + done, count - done);
+void write_all(int fd, const char* buffer, size_t len) {
+  for (; len > 0;) {
+    ssize_t bytes = write(fd, buffer, len);
     if (bytes == -1) {
       err(EXIT_FAILURE, "write");
     }
-    done += bytes;
+    buffer += bytes;
+    len -= bytes;
   }
 }
 
-void* reader(void* arg) {
-  int sock = (int)(int64_t)arg;
-  char buffer[4096];
-  ssize_t bytes;
-  while ((bytes = recv(sock, buffer, sizeof(buffer), /*flags=*/0)) > 0) {
+void* read_response(void* arg) {
+  int* server_fd = arg;
+
+  for (;;) {
+    char buffer[128];
+    ssize_t bytes = read(*server_fd, buffer, sizeof(buffer));
+    if (bytes == -1) {
+      err(EXIT_FAILURE, "read");
+    } else if (bytes == 0) {
+      break;
+    }
+
     write_all(STDOUT_FILENO, buffer, bytes);
   }
-  if (bytes == -1) {
-    err(EXIT_FAILURE, "recv");
-  }
 
-  shutdown(sock, SHUT_RD);
-  return NULL;
-}
-
-void* writer(void* arg) {
-  int sock = (int)(int64_t)arg;
-  char buffer[4096];
-  ssize_t bytes;
-  while ((bytes = read(STDIN_FILENO, buffer, sizeof(buffer))) > 0) {
-    send_all(sock, buffer, bytes);
-  }
-  if (bytes == -1) {
-    err(EXIT_FAILURE, "read");
-  }
-
-  shutdown(sock, SHUT_WR);
   return NULL;
 }
 
 int main(int argc, const char* argv[]) {
   if (argc != 3) {
-    fprintf(stderr, "usage: %s HOST PORT\n", argv[0]);
+    printf("usage: %s <ip> <port>\n", argv[0]);
     return EXIT_FAILURE;
   }
 
   const char* ip_str = argv[1];
   const char* port_str = argv[2];
-  int sock = dial_tcp(ip_str, port_str);
 
-  pthread_t r;
-  pthread_create(&r, /*attr=*/NULL, &reader, (void*)(int64_t)sock);
-  pthread_t w;
-  pthread_create(&w, /*attr=*/NULL, &writer, (void*)(int64_t)sock);
-  pthread_join(r, /*thread_return=*/NULL);
-  pthread_join(w, /*thread_return=*/NULL);
+  struct in_addr ip;
+  if (inet_aton(ip_str, &ip) == -1) {
+    err(EXIT_FAILURE, "inet_aton");
+  }
 
-  close(sock);
-  return EXIT_SUCCESS;
+  in_port_t port = atoi(port_str);
+
+  int server_fd = new_socket();
+
+  struct sockaddr_in server_addr = {
+      .sin_family = AF_INET,
+      .sin_addr = ip,
+      .sin_port = htons(port),
+  };
+
+  if (connect(server_fd, (const struct sockaddr*)&server_addr,
+              sizeof(server_addr)) == -1) {
+    err(EXIT_FAILURE, "connect");
+  }
+
+  pthread_t reader;
+  pthread_create(&reader, NULL, &read_response, &server_fd);
+
+  for (;;) {
+    char buffer[128];
+    ssize_t bytes = read(STDIN_FILENO, buffer, sizeof(buffer));
+
+    if (bytes == -1) {
+      err(EXIT_FAILURE, "read");
+    } else if (bytes == 0) {
+      break;
+    }
+
+    write_all(server_fd, buffer, bytes);
+  }
+
+  shutdown(server_fd, SHUT_RDWR);
+  close(server_fd);
+
+  pthread_join(reader, NULL);
+
+  return 0;
 }
--- net/tcp/server.c
+++ net/tcp/server.c
@@ -1,92 +1,81 @@
-#include <err.h>
-#include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
+#include <err.h>
 #include <unistd.h>
 
-void send_all(int sock, char* buffer, size_t count) {
-  size_t done = 0;
-  while (done < count) {
-    ssize_t bytes = send(sock, buffer + done, count - done, /*flags=*/0);
-    if (bytes == -1) {
-      err(EXIT_FAILURE, "send");
-    }
-    done += bytes;
-  }
-}
+#include <arpa/inet.h>
+#include <sys/un.h>
 
-int init_tcp(const char* ip_str, const char* port_str) {
-  int server_fd;
-  if ((server_fd = socket(AF_INET, SOCK_STREAM, /*protocol=*/0)) == -1) {
+int listen_tcp(struct in_addr ip, in_port_t port) {
+  int fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (fd == -1) {
     err(EXIT_FAILURE, "socket");
   }
 
-  struct in_addr ip;
-  if (inet_aton(ip_str, &ip) == 0) {
-    fprintf(stderr, "bad ip address\n");
-    exit(EXIT_FAILURE);
-  }
-  char* end_ptr;
-  in_port_t port = strtol(port_str, &end_ptr, /*base=*/10);
-  if (*end_ptr != '\0') {
-    fprintf(stderr, "bad tcp port");
-    exit(EXIT_FAILURE);
-  }
-  struct sockaddr_in server_addr = {
-      .sin_family = AF_INET, .sin_addr = ip, .sin_port = htons(port)};
-  if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) ==
-      -1) {
+  struct sockaddr_in addr = {
+      .sin_family = AF_INET,
+      .sin_addr = ip,
+      .sin_port = htons(port),
+  };
+
+  if (bind(fd, (const struct sockaddr*)&addr, sizeof(addr)) == -1) {
     err(EXIT_FAILURE, "bind");
   }
 
-  if (listen(server_fd, SOMAXCONN) == -1) {
+  if (listen(fd, SOMAXCONN) == -1) {
     err(EXIT_FAILURE, "listen");
   }
 
-  return server_fd;
-}
-
-int accept_client(int server_fd) {
-  struct sockaddr_in client_addr = {};
-  socklen_t len = sizeof(client_addr);
-  int client_fd;
-  if ((client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &len)) ==
-      -1) {
-    err(EXIT_FAILURE, "accept");
-  }
-
-  return client_fd;
+  return fd;
 }
 
 int main(int argc, const char* argv[]) {
   if (argc != 3) {
-    fprintf(stderr, "usage: %s ip port\n", argv[0]);
+    printf("usage: %s <ip> <port>\n", argv[0]);
     return EXIT_FAILURE;
   }
 
   const char* ip_str = argv[1];
   const char* port_str = argv[2];
-  int server_fd = init_tcp(ip_str, port_str);
 
-  while (true) {
-    int client_fd = accept_client(server_fd);
+  struct in_addr ip;
+  if (inet_aton(ip_str, &ip) == -1) {
+    err(EXIT_FAILURE, "inet_aton");
+  }
 
-    char buffer[4096];
-    ssize_t bytes;
-    while ((bytes = recv(client_fd, buffer, sizeof(buffer), /*flags=*/0)) > 0) {
-      send_all(client_fd, buffer, bytes);
+  in_port_t port = atoi(port_str);
+
+  int server_fd = listen_tcp(ip, port);
+
+  struct sockaddr_in client_addr;
+  socklen_t client_addr_len = sizeof(client_addr);
+
+  for (;;) {
+    int client_fd =
+        accept(server_fd, (struct sockaddr*)&client_addr, &client_addr_len);
+    if (client_fd == -1) {
+      err(EXIT_FAILURE, "accept");
     }
-    if (bytes == -1) {
-      err(EXIT_FAILURE, "recv");
+
+    printf("client connected ip=%s port=%d\n", inet_ntoa(client_addr.sin_addr),
+           client_addr.sin_port);
+
+    char msg[] = "hello\n";
+    if (write(client_fd, msg, sizeof(msg)) == -1) {
+      err(EXIT_FAILURE, "write");
+    }
+
+    for (;;) {
+      char buffer[128];
+      ssize_t bytes = read(client_fd, buffer, sizeof(buffer));
+      if (bytes == -1) {
+        err(EXIT_FAILURE, "read");
+      } else if (bytes == 0) {
+        break;
+      }
     }
 
     shutdown(client_fd, SHUT_RDWR);
     close(client_fd);
   }
-
-  close(server_fd);
-  return EXIT_SUCCESS;
 }
